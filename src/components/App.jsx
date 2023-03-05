9198118201ec938df5c8393ce0c8717ecc9e428c
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2023-03-05 13:55:36 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Section } from "./main.styled";
+import allUsers from "../data/allUsers.json";
+import { UserGallery } from "./UserGallery/UserGallery";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Section>
+      <UserGallery users={allUsers} />
+    </Section>
   );
 };
